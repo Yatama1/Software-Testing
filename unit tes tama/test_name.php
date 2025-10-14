ae7b62c54693d6cdd40c950a06d4d38e1b0ad17c
Yatama1 <adityadwikitama55@gmail.com>
2025-10-14 18:43:36 +0700

    add integration

--- unit tes tama/test_name.php
+++ unit tes tama/test_name.php
@@ -4,7 +4,7 @@ require 'validator.php';
 
 // test case 1 - valid age
 try{
-    $result = validateName("111111");
+    $result = validateName(111111);
     echo "PASS : Nama Masuk akal.\n";
 }catch (Exception $e){
     echo "FAIL : Nama Tidak masuk akal. Error : ". $e->getMessage() ."\n";
